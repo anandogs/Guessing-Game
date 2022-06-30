6ba30f2b02fe4d8a837588b8d5eda689da630581
Anando Ghose <anandoghose@gmail.com>
2022-06-30 14:03:49 +0530

    added count

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
+    let mut count: u32 = 0;
+
     loop {
         println!("Please input a number");
         
@@ -23,12 +24,19 @@ fn main() {
         println!("You guessed {}", guess);
         
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too Small!"),
+            Ordering::Less => { 
+                println!("Too Small!");
+                count = count + 1;
+            }
             Ordering::Equal => { 
                 println!("You Win!");
+                println!("Your score is {}. The lower the better!", count);
                 break;
             }
-            Ordering::Greater => println!("Too Big!"),
+            Ordering::Greater => {
+                println!("Too Big!");
+                count = count + 1;
+            }
         }
     }
         
